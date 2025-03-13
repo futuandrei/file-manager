8348ffcdedd602b590812500a252b97524692b04
Andrei Savolainen <andrei.savolainen@gmail.com>
2025-03-13 11:31:15 +0200

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 // import FileUpload from "./components/FileUpload";
 // import FilesTable from "./components/FilesTable";
 import FileManager from "./components/FileManager";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>File manager</h1>
+      <Header />
       <FileManager />
     </div>
   );
